3bfe16395d2a7b6793d23a61c1cf7c0766af773d
samibutt77 <samib8104@gmail.com>
2025-07-13 20:15:52 +0500

    Add files via upload

--- abtls/internal/tlsprofile/profiles.go
+++ abtls/internal/tlsprofile/profiles.go
@@ -8,21 +8,34 @@ import (
 
 // GetClientHello returns the ClientHelloSpec and the profile label used.
 func GetClientHello(profile string) (utls.ClientHelloSpec, string) {
+	addHTTP2ALPN := func(spec utls.ClientHelloSpec) utls.ClientHelloSpec {
+		// Append or replace ALPNExtension with h2 and http/1.1
+		spec.Extensions = append(spec.Extensions, &utls.ALPNExtension{
+			AlpnProtocols: []string{"h2", "http/1.1"},
+		})
+		return spec
+	}
+
 	switch profile {
 	case "chrome":
 		spec, _ := utls.UTLSIdToSpec(utls.HelloChrome_Auto)
+		spec = addHTTP2ALPN(spec)
 		return spec, "chrome"
+
 	case "firefox":
 		spec, _ := utls.UTLSIdToSpec(utls.HelloFirefox_Auto)
+		spec = addHTTP2ALPN(spec)
 		return spec, "firefox"
+
 	case "safari":
+		// Safari typically doesn't negotiate HTTP/2 explicitly in some versions
 		spec, _ := utls.UTLSIdToSpec(utls.HelloSafari_Auto)
 		return spec, "safari"
+
 	case "random":
-		// Randomly pick one of the presets
 		presets := []struct {
-			ID     utls.ClientHelloID
-			Label  string
+			ID    utls.ClientHelloID
+			Label string
 		}{
 			{utls.HelloChrome_Auto, "chrome"},
 			{utls.HelloFirefox_Auto, "firefox"},
@@ -30,7 +43,11 @@ func GetClientHello(profile string) (utls.ClientHelloSpec, string) {
 		}
 		pick := presets[rand.Intn(len(presets))]
 		spec, _ := utls.UTLSIdToSpec(pick.ID)
+		if pick.Label == "chrome" || pick.Label == "firefox" {
+			spec = addHTTP2ALPN(spec)
+		}
 		return spec, pick.Label
+
 	default:
 		spec, _ := utls.UTLSIdToSpec(utls.HelloRandomized)
 		return spec, "randomized"
